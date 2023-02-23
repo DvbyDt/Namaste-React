8eac04d29f4426bfe13223a712863afae8375aa1
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2023-02-23 22:04:37 +0530

    Learning from Let's get Classy
    This commit contains learning from Let's get classy module were we learnt about class based components it's lifecycle methods and comparison of those class based components with that of new functional based components. The order of execution in lifecycle methods and the lifecycle diagram.

---
+++ Let's Get Classy/src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return(
+        <>
+            <h1>Contact Us</h1>
+            <p> Feel free to reach out to me in case of any queries</p>
+        </>
+    )
+}
+
+export default Contact
